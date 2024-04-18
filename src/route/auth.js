3adea5dcc0963ea2ec0385de3b68c1b8c9ba8d2d
StanislavRadas <stasradysh@gmail.com>
2024-04-18 18:56:02 +0200

    part2

--- src/route/auth.js
+++ src/route/auth.js
@@ -5,6 +5,12 @@ const router = express.Router()
 
 const { User } = require('../class/user')
 
+User.create({
+  email: 'test@mail.com',
+  pasword: 123,
+  role: 1,
+})
+
 // ================================================================
 
 // router.get Створює нам один ентпоїнт
@@ -41,6 +47,27 @@ router.get('/signup', function (req, res) {
   })
   // ↑↑ сюди вводимо JSON дані
 })
+router.post('/signup', function (req, res) {
+  const { email, password, role } = req.body;
 
+  console.log(req.body)
+
+  if (!email || !password || !role) {
+    return res.status(400).json({
+      message: 'Error! Requried fields are empty!',
+    })
+  }
+  try {
+    User.create({email, password, role})
+
+    return res.status(200).json({
+      message: 'User is created!'
+    })
+  } catch (err) {
+    return res.status(400).json({
+      message: 'Error! User is not created!',
+    })
+  }
+})
 // Підключаємо роутер до бек-енду
 module.exports = router
